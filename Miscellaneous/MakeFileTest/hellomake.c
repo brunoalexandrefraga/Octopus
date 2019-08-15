eb8ae5772fc9bd1e4f16c3c496e570f6b9a35b11
Fraga <brunoalexandrefraga@gmail.com>
2019-08-16 06:48:48 +0900

    Create basic project to test makefile and print a octopus ascii art

---
+++ Miscellaneous/MakeFileTest/hellomake.c
@@ -0,0 +1,26 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  hellomake.c
+ *
+ *    Description:  Example to use makefile
+ *
+ *        Version:  1.0
+ *        Created:  16/08/2019 05:56:07
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Dr. Fritz Mehner (mn), mehner@fh-swf.de
+ *        Company:  FH Südwestfalen, Iserlohn
+ *
+ * =====================================================================================
+ */
+
+#include <hellomake.h>
+
+int main() {
+  // call a function in another file
+  myPrintHelloMake();
+
+  return(0);
+}
---
+++ Miscellaneous/MakeFileTest/hellomake.h
@@ -0,0 +1,23 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  hellomake.h
+ *
+ *    Description:  Header to test makefile
+ *
+ *        Version:  1.0
+ *        Created:  16/08/2019 05:57:29
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Dr. Fritz Mehner (mn), mehner@fh-swf.de
+ *        Company:  FH Südwestfalen, Iserlohn
+ *
+ * =====================================================================================
+ */
+
+/*
+example include file
+*/
+
+void myPrintHelloMake(void);
